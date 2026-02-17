4258e3eee29a6e21a102237cfa8bdfaa2e5e3879
EmberHollowood <annabethchasex18@gmail.com>
2026-02-17 15:29:24 +0530

    Update UC4.java
    Removed comments explaining the string array and the for-loop.

--- UC4.java
+++ UC4.java
@@ -2,8 +2,6 @@ public class UC4 {
     public static void main(String[] args) {
         System.out.println("OOPS using String Array and Enhanced For-Loop:\n");
         
-        // Store all banner lines in a String array
-        // Each line contains ASCII art patterns for the letters O, O, P, S
         String[] bannerLines = {
             String.join(" ", "   ***   ", "   ***    " , "*********  " , "   ***** "),
             String.join(" ", " **   ** ", " **   **  " , "**     **  " , " **     "),
@@ -14,7 +12,6 @@ public class UC4 {
             String.join(" ", "   ***   ", "   ***    " , "**         " , "  ***** ")
         };
         
-        // Print banner using enhanced for-loop
         for (String line : bannerLines) {
             System.out.println(line);
         }
