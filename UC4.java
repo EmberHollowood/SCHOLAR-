295f165971d5c54755a67ee29a41d7651bc474aa
EmberHollowood <annabethchasex18@gmail.com>
2026-02-17 15:28:05 +0530

    Remove comments from UC4.java
    Removed comments describing the program's functionality.

--- UC4.java
+++ UC4.java
@@ -1,12 +1,4 @@
-/**
- * UC4 - OOPS Banner App using Arrays and Loops
- * 
- * This program improves upon UC3 by:
- * - Storing banner lines in a String array
- * - Using enhanced for-loop to print them
- * - Achieving better modularity and reusability
- * - Reducing code repetition
- */
+
 public class UC4 {
     public static void main(String[] args) {
         System.out.println("OOPS using String Array and Enhanced For-Loop:\n");
